12ffbf8b8fe710d95a7be4815b1dae8a5fa8d1de
Siddharth <siddharth.060605@gmail.com>
2025-02-15 13:51:24 -0600

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  outputs =
+    { nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          meson
+          ninja
+        ];
+      };
+    };
+}
